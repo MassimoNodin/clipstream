7a2982e6b8f77770142d78267e80129dcd9bd670
MassimoNodin <massimonodin@gmail.com>
2025-08-06 21:47:27 +1000

    feat: integrate NextAuth for authentication and add AuthButton component

---
+++ web/clipstream/src/app/providers/NextAuthProvider.tsx
@@ -0,0 +1,8 @@
+'use client'; // must be a client component
+
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react';
+
+export default function NextAuthProvider({ children }: { children: ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
